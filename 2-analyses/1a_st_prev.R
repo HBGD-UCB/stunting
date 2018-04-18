239d4430d17df94d77b5feeab5c25deb49186b22
Jade Benjamin-Chung <jade.benjamin-chung@PROD.GHAP.IO>
2018-04-18 15:53:13 +0000

    reading se source fn

--- 2-analyses/1a_st_prev.R
+++ 2-analyses/1a_st_prev.R
@@ -12,24 +12,14 @@ rm(list=ls())
 library(dplyr)
 library(ggplot2)
 library(tidyr)
-# library(tidyverse)
-# library(caret)
-# library(MASS)
-# library(reshape2)
-# library(zoo)
-# library(epitools)
 library(binom)
 theme_set(theme_bw())
 
 
-# source("Wast_incidence_functions.R") #need to write
 setwd("U:/data/GHAP_data/")
 
-# # pooling across studies
-# x=data.frame(a=c(1,1),b=c(1,1))
-# y=data.frame(a=c(1,1),b=c(1,1))
-# 
-# x=do.call(rbind(x,y))
+# load standard error function
+source("U:/Scripts/Stunting/2-analyses/0_se_fns.R")
 
 # NO MLEX CHECK WITH ANDREW
 
@@ -64,71 +54,7 @@ data.set=lapply(study.list,data.prep)
 all.data=do.call(rbind,data.set)
 nrow(all.data)
 
-#----------------------------------------------
-# Mean and 95% CI function 
-# IS THIS THE RIGHT FUNCTION?
-#----------------------------------------------
-mean95CI <- function(Y, id=rep(1:length(Y)), 
-             persontime=NULL, proportion=F, percent=F, count=F){
-  
-  if(proportion==F){
-    if(count==T){
-      IR.CI <- pois.exact(Y, pt = persontime, conf.level = 0.95)[3:5] 
-      mean_ci <- data.frame(N=Y, Mean=IR.CI[1], SD=NA, Robust.SE=NA ,  Lower.95.CI=IR.CI[2] ,  Upper.95.CI=IR.CI[3] )
-      colnames(mean_ci) <- c("N","Mean","SD","Robust SE", "Lower 95%CI", "Upper 95%CI") 
-    }else{
-      if(!is.na(mean(Y[complete.cases(Y)]))){
-        mudat <- data.frame(id = id, Y = Y)
-        mudat <- mudat[complete.cases(mudat), ]
-        n.sub <- dim(mudat)[1]
-        fit <- glm(Y ~ 1, family = gaussian, data = mudat)
-        vcovCL <- sandwichSE(mudat, fm = fit, cluster = mudat$id)
-        rfit <- coeftest(fit, vcovCL)
-        lb <- rfit[1, 1] - 1.96 * rfit[1, 2]
-        ub <- rfit[1, 1] + 1.96 * rfit[1, 2]
-        mean_ci <- matrix(c(n.sub, rfit[1, 1], sd(mudat$Y), rfit[1, 
-                                                                 2], lb, ub), nrow = 1, ncol = 6)
-        colnames(mean_ci) <- c("N", "Mean", "SD", "Robust SE", "Lower 95%CI", 
-                               "Upper 95%CI")
-        
-      }else{
-        mean_ci <- data.frame(N=NA, Mean=NA, SD=NA, `Robust SE`=NA, `Lower 95%CI`=NA, `Upper 95%CI`=NA)
-        colnames(mean_ci) <- c("N", "Mean", "SD", "Robust SE", "Lower 95%CI", "Upper 95%CI")  
-      }
-    }
-  }else{
-    
-    require(binom)
-    # Find the number of obs
-    n = length(Y[!is.na(Y)])
-    if(percent==T){
-      CR.res<-binom.confint(sum(Y/100, na.rm = T), n, method="exact")
-    }else{
-      CR.res<-binom.confint(sum(Y, na.rm = T), n, method="exact")
-    }
-    mean_ci <- data.frame(N=n, Mean=CR.res[4], SD=NA, `Robust SE`=NA, `Lower 95%CI`=CR.res[5], `Upper 95%CI`=CR.res[6])
-    colnames(mean_ci) <- c("N", "Mean", "SD", "Robust SE", "Lower 95%CI", "Upper 95%CI")  
-  }
-  return(mean_ci)
-}
 
-#Function to calculate the robust SEs
-sandwichSE <- function (dat, fm, cluster) 
-{
-  require(sandwich, quietly = TRUE)
-  require(lmtest, quietly = TRUE)
-  if (is.factor(cluster)) {
-    cluster <- droplevels(cluster)
-  }
-  M <- length(unique(cluster))
-  N <- length(cluster)
-  K <- fm$rank
-  dfc <- (M/(M - 1)) * ((N - 1)/(N - K))
-  uj <- apply(estfun(fm), 2, function(x) tapply(x, cluster, 
-                                                sum))
-  vcovCL <- dfc * sandwich(fm, meat = crossprod(uj)/N)
-  return(vcovCL)
-}
 
 # define age windows
 all.data = all.data %>% 
