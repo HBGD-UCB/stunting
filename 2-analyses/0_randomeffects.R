4e49725aff82e603e9c934cefe10af0d8d6dced9
PROD\jade.benjamin-chung <jadebc@berkeley.edu>
2018-04-18 23:18:41 +0000

    my random effects fn

---
+++ 2-analyses/0_randomeffects.R
@@ -0,0 +1,17 @@
+# random effects function, save results nicely
+fit.rma=function(data,age,ni,xi){
+  data=filter(data,agecat==age)
+  fit<-rma(ni=data[[ni]], xi=data[[xi]], 
+           method="REML", measure="PR")
+  out=data %>%
+    ungroup() %>%
+    summarise(nstudies=length(unique(studyid)),
+              nmeas=sum(data[[ni]][agecat==age])) %>%
+    mutate(agecat=age,est=fit$beta, se=fit$se, lb=fit$ci.lb, ub=fit$ci.ub,
+           nmeas.f=paste0("N=",format(sum(data[[ni]]),big.mark=",",scientific=FALSE),
+                          " measurements"),
+           nstudy.f=paste0("N=",nstudies," studies"))
+  return(out)
+}
+
+
