467b84e1be4675a602e91d6dbea31ec18ef0cb23
PROD\jade.benjamin-chung <jadebc@berkeley.edu>
2018-04-19 21:24:18 +0000

    added random effects

--- 2-analyses/1a_st_inc.R
+++ 2-analyses/1a_st_inc.R
@@ -8,17 +8,6 @@
 # calculated with exact binomial test - Pearson 
 # and Klopper method
 #-----------------------------------
-# Incident stunting episodes will be defined as a 
-# change in LAZ from above -2 z in the prior measurement to below -2 z in 
-# the current measurement. Children will only be considered 
-# “at risk” for incident stunting episodes, and thus contribute person-time at 
-# risk for stunting, if they currently have a z-score ≥ -2 and previously did 
-# not have a z-score < -2. Since linear growth faltering that leads to stunting 
-# is a cumulative process, if a child’s LAZ score fluctuates above and below -2, 
-# we will consider the first measurement below -2 as the incident onset of 
-# stunting and then following that the child will no longer be included in the 
-# risk set for stunting.
-
 rm(list=ls())
 library(dplyr)
 library(ggplot2)
@@ -27,8 +16,8 @@ library(binom)
 library(epitools)
 theme_set(theme_bw())
 
-# load standard error function
-source("U:/Scripts/Stunting/2-analyses/0_se_fns.R")
+# load random effects function
+source("U:/Scripts/Stunting/2-analyses/0_randomeffects.R")
 
 load("U:/Data/Stunting/stunting_data.RData")
 
@@ -49,68 +38,99 @@ all.data %>%
             mean=mean(agedays/30.4167),
             max=max(agedays/30.4167))
 
-# define risk set: current HAZ>=-2, previous HAZ>=-2 in same time window
-# identify ever stunted children
-evs = all.data %>%
+# ---------------------------------------
+# flag incident cases and define risk set
+# ---------------------------------------
+inc.prep = all.data %>%
   filter(!is.na(agecat)) %>%
   group_by(studyid,subjid) %>%
   arrange(studyid,subjid,agedays) %>%
+  
   # create id for measurement within person
   mutate(measid=seq_along(subjid)) %>%
-  # create variable with haz at t-1
-  mutate(hazprev=lag(haz)) %>%
+  # duration between measurements 
+  mutate(agedayslag=lag(agedays)) %>%
+  mutate(agedayslag=ifelse(is.na(agedayslag),0,agedayslag)) %>%
+  mutate(deltat=ifelse(measid==1 & agecat=="Birth",0,agedays-agedayslag)) %>%
+
   # create indicator for whether haz at t < haz at t-1
-  mutate(newcase=ifelse(measid==1,
-                    ifelse(haz< -2, 1,0),
-                    ifelse(hazprev>= -2 & haz< -2,1,0))) %>%
-  # create at risk variable
-  mutate(atrisk=ifelse(agecat=="Birth",1,1-cumsum(newcase))) 
+  mutate(hazlag=lag(haz)) %>%
+  mutate(newcase=ifelse(measid==1,ifelse(haz< -2, 1,0),
+                    ifelse(hazlag>= -2 & haz< -2,1,0))) %>%
+  mutate(newcaselag=lag(newcase))%>%
+  mutate(newcaselag=ifelse(measid==1,0,newcaselag))%>%
+  mutate(cnewcaselag=cumsum(newcaselag)) %>%
   
-# # calculate incidence at birth
-# inc.birth = evs %>%
-#   ungroup() %>%
-#   filter(agecat=="Birth" & atrisk==1) %>%
-#   summarise(inc.cases=sum(newcase),
-#             at.risk=sum(atrisk)) 
-# 
-# inc1=c("Birth",pois.exact(inc.birth$inc.cases,inc.birth$at.risk)[c("rate","lower","upper")])
-#         # getting an error when using mean95CI fn
-# 
-# # calculate incidence at 6 months
-# inc.age = evs %>%
-#   filter(agecat!="Birth") %>%
-#   group_by(agecat) %>%
-#   summarise(inc.cases=sum(newcase),
-#             at.risk=sum(atrisk)) 
-# 
-# inc2=cbind(inc.age$agecat,pois.exact(inc.age$inc.cases,inc.age$at.risk)[,c("rate","lower","upper")])
-# names(inc1)=colnames(inc2)
-# inc.res=rbind(inc1,inc2)
-# colnames(inc.res)=c("agecat","rate","lb","ub")
+  # create at risk variable
+  mutate(atrisk=ifelse(cnewcaselag>=1,0,1)) %>%
+  # create inc case variable
+  mutate(inccase=ifelse(cnewcaselag>=1,0,newcase)) %>%
+    
+  # clean up
+  select(-c(hazlag,newcase,newcaselag, cnewcaselag,agedayslag))
 
-inc.data = evs %>%
-  group_by(agecat) %>%
-  summarise(ptar=sum(!is.na(atrisk)),
-          nstudy=length(unique(studyid)),
-          prev=mean(stunted),
-          lb=mean95CI(newcase,atrisk,proportion=F,count=T)[["Lower 95%CI"]],
-          ub=mean95CI(newcase,atrisk,proportion=F,count=T)[["Upper 95%CI"]],
-          measure="Stunting") %>%
-  mutate(nmeas.f=paste0("N=",format(nmeas,big.mark=",",scientific=FALSE),
-                        " measurements"),
-         nstudy.f=paste0("N=",nstudy," studies"))
 
-mean95CI(Y=evs$newcase,persontime=evs$atrisk,proportion=F,count=T)
+inc.prep[inc.prep$studyid=="ki1000108-CMC-V-BCS-2002"& inc.prep$agecat=="Birth",
+    c("subjid","measid","agedays","haz","deltat")][1:20,]
+
+
+
+inc.prep[inc.prep$studyid=="ki1000110-WASH-Bangladesh"& inc.prep$agecat=="6 months",
+         c("subjid","measid","agedays","haz","deltat")][1:20,]
+
+
+inc.prep[inc.prep$studyid=="ki1000110-WASH-Bangladesh" & inc.prep$subjid==1610800,
+         c("subjid","agedays","agedayslag","haz","deltat")]
+
+
+# count incident cases and sum person time at risk per study by age
+# exclude time points if number of children per age
+# in a study is <50  
+
+# at birth
+inc.data.0 = inc.prep %>%
+  filter(agecat=="Birth") %>%
+  group_by(studyid) %>%
+  summarise(ptar=sum(atrisk),
+            ncase=sum(inccase),
+            nchild=length(unique(subjid)),
+            nstudy=length(unique(studyid))) %>%
+  filter(nchild>=50) %>%
+  mutate(agecat="Birth") %>%
+  select(studyid,agecat,everything())
+
+# not at birth
+inc.data.1 = inc.prep %>%
+  filter(agecat!="Birth") %>%
+  group_by(studyid,agecat) %>%
+  summarise(ptar=sum(deltat*atrisk),
+            ncase=sum(inccase),
+            nchild=length(unique(subjid)),
+            nstudy=length(unique(studyid))) %>%
+  filter(nchild>=50)
+
+inc.data=bind_rows(inc.data.0,inc.data.1) %>%
+  mutate(agecat=factor(agecat,levels=c("Birth","6 months","12 months","24 months"))) %>%
+  arrange(studyid,agecat)
+          
+# estimate random effects, format results
+ir.res=lapply(list("Birth","6 months","12 months","24 months"),function(x)
+  fit.rma(data=inc.data,ni="ptar", xi="ncase",age=x))
+ir.res=as.data.frame(do.call(rbind, ir.res))
+ir.res[,4]=as.numeric(ir.res[,4])
+ir.res$agecat=factor(ir.res$agecat,levels=c("Birth","6 months","12 months","24 months"))
+
+ir.res
 
 pdf("U:/Figures/stunting-inc-pool.pdf",width=8,height=4,onefile=TRUE)
-ggplot(inc.res,aes(y=rate,x=agecat))+
+ggplot(ir.res,aes(y=est*1000,x=agecat))+
   geom_point()+
-  geom_errorbar(aes(ymin=lb,ymax=ub),width=0.05) +
+  geom_errorbar(aes(ymin=lb*1000,ymax=ub*1000),width=0.05) +
   scale_color_manual(values=tableau10)+xlab("Age category")+
-  ylab("Incidence rate (95% CI)")+
-  scale_y_continuous(limits=c(0,0.3))+
-  annotate("text",x=sprev.data$agecat,y=0.03,label=sprev.data$nmeas.f,size=3)+
-  annotate("text",x=sprev.data$agecat,y=0.01,label=sprev.data$nstudy.f,size=3)
+  ylab("Incidence rate per 1,000 child-days (95% CI)")+
+  scale_y_continuous(limits=c(0,18))+
+  annotate("text",x=ir.res$agecat,y=0.02,label=ir.res$nmeas.f,size=3)+
+  annotate("text",x=ir.res$agecat,y=0.005,label=ir.res$nstudy.f,size=3)
 dev.off()
 
 
