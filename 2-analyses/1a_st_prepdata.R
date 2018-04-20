cfd9118517e29f273a88ddbd96798a28b2357f7c
PROD\jade.benjamin-chung <jadebc@berkeley.edu>
2018-04-20 23:42:28 +0000

    revise study subset, add scatter plots

--- 2-analyses/1a_st_prepdata.R
+++ 2-analyses/1a_st_prepdata.R
@@ -10,7 +10,6 @@ library(data.table)
 setwd("U:/data/GHAP_data/")
 d<-fread("U:/data/Stunting/Full-compiled-data/FINAL.csv", header = T)
 
-
 #--------------------------------------------
 # Subset to relevant variables
 #--------------------------------------------
@@ -27,6 +26,14 @@ d = filter(d,haz >= -6 & haz <=6)
 nrow(d)
 
 #--------------------------------------------
+# plot HAZ by agedays for all studies
+#--------------------------------------------
+pdf("U:/Figures/haz-scatter-all.pdf",width=15,height=15,onefile=TRUE)
+ggplot(d[d$agedays<=365*2,],aes(x=agedays,y=haz))+geom_point(alpha=0.3)+geom_smooth()+
+  facet_wrap(~studyid+country)+geom_hline(yintercept=-2,linetype="dashed",col="red")
+dev.off()
+
+#--------------------------------------------
 # order data, create measurement id
 #--------------------------------------------
 d <- d %>% 
@@ -37,7 +44,7 @@ d <- d %>%
   mutate(measid=seq_along(subjid)) 
 
 #--------------------------------------------
-# calculate average months between measurements
+# assess frequency of measurement
 #--------------------------------------------
 d <- d %>%
   arrange(studyid,subjid,agedays) %>%
@@ -49,14 +56,41 @@ d <- d %>%
   mutate(agedayslag=ifelse(is.na(agedayslag),0,agedayslag),
          deltam=ifelse(is.na(deltam)|measid==1,0,deltam))
 
+# calculate average months between measurements
 avgmths <- d %>%
-  group_by(studyid) %>%
+  group_by(studyid,country) %>%
   summarise(meantime=mean(deltam)) %>%
   arrange(meantime)
 
+medmths <- d %>%
+  group_by(studyid,country) %>%
+  summarise(medtime=median(deltam)) %>%
+  arrange(medtime)
+
+
+# calculate # measurements per child under 24 months
+nmeas <- d %>%
+  group_by(studyid,country,subjid) %>%
+  filter(agedays<=365*2) %>%
+  summarise(nmeas=n()) %>%
+  group_by(studyid,country) %>%
+  summarise(mnmeas=mean(nmeas)) %>%
+  arrange(mnmeas)
+
+# combine 
+comb=full_join(avgmths,nmeas,by=c("studyid","country")) 
+comb=full_join(comb,medmths,by=c("studyid","country")) 
+
+# candidates for dropping
+comb[comb$meantime>3 | comb$mnmeas<5,]
+
 # subset to studies with data collection at least every 3 months
 drops=c("ki1148112-iLiNS-DYAD-M","ki1148112-iLiNS-DOSE",
-        "ki1000111-WASH-Kenya","ki1000110-WASH-Bangladesh")
+        "ki1000111-WASH-Kenya","ki1000110-WASH-Bangladesh",
+        "ki1000107-Serrinha-VitA","ki1000304-VITAMIN-A",
+        "ki1000304-Vitamin-B12","ki1000301-DIVIDS",
+        "ki1112895-Guatemala BSC","ki1000304-ZnMort",
+        "ki1000304-EU")
 
 d<- d %>%
   filter(!studyid %in% drops) 
@@ -66,6 +100,8 @@ drops=which(d$studyid=="ki1135781-COHORTS" & (
   d$country=="BRAZIL"|d$country=="INDIA"|
     d$country=="SOUTH AFRICA"))
 
+# drop cmin guinnea and cohorts guatemala trials since no arm data
+
 d=d[-drops,]
 rm(drops)
 
@@ -74,17 +110,24 @@ rm(drops)
 # potentially subset cmin and cohorts to control too,
 # but currently there is no tr variable for them
 #--------------------------------------------
-d=d[-which(d$studyid=="ki1000304-EU" & d$tr=="Zinc"),]
 d=d[-which(d$studyid=="kiGH5241-JiVitA-4" & d$tr!="Control"),]
 d=d[-which(d$studyid=="ki1119695-PROBIT" & d$tr!="Control"),]
 d=d[-which(d$studyid=="ki1000304b-SAS-FoodSuppl" & d$tr!="Control"),]
-d=d[-which(d$studyid=="ki1000304-VITAMIN-A" & d$tr!="Control"),]
 
 # count number of studies
 length(names(table(d$studyid)))
 
 # table of studies
 table(d$studyid)
+
+#--------------------------------------------
+# plot HAZ by agedays for included studies
+#--------------------------------------------
+pdf("U:/Figures/haz-scatter-1a.pdf",width=15,height=15,onefile=TRUE)
+ggplot(d[d$agedays<=365*2,],aes(x=agedays,y=haz))+geom_point(alpha=0.3)+geom_smooth()+
+  facet_wrap(~studyid+country)+geom_hline(yintercept=-2,linetype="dashed",col="red")
+dev.off()
+
 
 save(d,file="U:/Data/Stunting/stunting_data.RData")
 
