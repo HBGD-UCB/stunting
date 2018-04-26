349ff975144d26fe90fc617f93f84c37f7a1446d
PROD\jade.benjamin-chung <jadebc@berkeley.edu>
2018-04-26 19:36:07 +0000

    added sensitivity analysis, plot titles

--- 2-analyses/1a_st_cuminc.R
+++ 2-analyses/1a_st_cuminc.R
@@ -87,4 +87,125 @@ ggplot(ci.res,aes(y=est,x=agecat))+
            y=ci.res$est,hjust=-0.3,size=3)
 dev.off()
 
+#--------------------------------------
+# Sensitivity analysis excluding birth from CI
+# only among birth cohort studies
+#--------------------------------------
+# birth cohorts
+bc= d %>% group_by(studyid,country) %>% 
+  summarise(minage=min(agedays)) %>%
+  # filter(minage==1) %>%
+  mutate(birthcohort=ifelse(minage==1,1,0)) %>%
+  select(-minage)
+  
+# identify ever stunted children
+# excluding birth in birth cohort studies
+evs.sens.nobirth = left_join(d, bc, by=c("studyid","country")) %>%
+  filter(birthcohort==1 & !is.na(agecat) & agedays>1) %>%
+  group_by(studyid,subjid) %>%
+  arrange(studyid,subjid) %>%
+  #create variable with minhaz by age category, cumulatively
+  mutate(minhaz=ifelse(agecat=="6 months",min(haz[agecat=="6 months"]),
+                       ifelse(agecat=="12 months",min(haz[agecat=="6 months"|agecat=="12 months"]),
+                              ifelse(agecat=="18 months",min(haz[agecat=="6 months"|agecat=="12 months"|agecat=="18 months"]),
+                                     min(haz))))) %>%
+  # create indicator for whether the child was ever stunted
+  # by age category
+  group_by(studyid,subjid,agecat) %>%
+  mutate(evst=ifelse(minhaz< -2,1,0)) 
 
+# count incident cases per study by age
+# exclude time points if number of measurements per age
+# in a study is <50  
+cuminc.data.nobirth= evs.sens.nobirth%>%
+  group_by(studyid,agecat) %>%
+  summarise(
+    nchild=length(unique(subjid)),
+    nstudy=length(unique(studyid)),
+    ncases=sum(evst),
+    N=sum(length(evst))) %>%
+  filter(N>=50)
+
+cuminc.data.nobirth
+
+# estimate random effects, format results
+ci.res.nobirth=lapply(list("6 months","12 months","18 months","24 months"),function(x)
+  fit.rma(data=cuminc.data.nobirth,ni="N", xi="ncases",age=x))
+ci.res.nobirth=as.data.frame(do.call(rbind, ci.res.nobirth))
+ci.res.nobirth[,4]=as.numeric(ci.res.nobirth[,4])
+ci.res.nobirth$agecat=factor(ci.res.nobirth$agecat,levels=c("6 months","12 months","18 months","24 months"))
+ci.res.nobirth$ptest.f=sprintf("%0.02f",ci.res.nobirth$est)
+
+ci.res.nobirth
+
+
+# plot pooled cumulative incidence
+pdf("U:/Figures/stunting-cuminc-pool-bc-birth.pdf",width=8,height=3,onefile=TRUE)
+ggplot(ci.res.nobirth,aes(y=est,x=agecat))+
+  geom_point(size=3)+
+  geom_errorbar(aes(ymin=lb,ymax=ub),width=0.05) +
+  scale_y_continuous(limits=c(0.13,0.8))+
+  xlab("Age category")+
+  ylab("Cumulative incidence (95% CI)")+
+  annotate("text",x=ci.res.nobirth$agecat,y=0.18,label=ci.res.nobirth$nmeas.f,size=3)+
+  annotate("text",x=ci.res.nobirth$agecat,y=0.15,label=ci.res.nobirth$nstudy.f,size=3)+
+  annotate("text",label=ci.res.nobirth$ptest.f,x=ci.res.nobirth$agecat,
+           y=ci.res.nobirth$est,hjust=-0.3,size=3)+
+  ggtitle("Pooled cumulative incidence of stunting - birth cohorts only - CI includes birth")
+dev.off()
+
+
+# identify ever stunted children
+# including birth in birth cohort studies
+evs.sens.birth = left_join(d, bc, by=c("studyid","country")) %>%
+  filter(birthcohort==1 & !is.na(agecat)) %>%
+  group_by(studyid,subjid) %>%
+  arrange(studyid,subjid) %>%
+  #create variable with minhaz by age category, cumulatively
+  mutate(minhaz=ifelse(agecat=="6 months",min(haz[agecat=="6 months"]),
+                       ifelse(agecat=="12 months",min(haz[agecat=="6 months"|agecat=="12 months"]),
+                              ifelse(agecat=="18 months",min(haz[agecat=="6 months"|agecat=="12 months"|agecat=="18 months"]),
+                                     min(haz))))) %>%
+  # create indicator for whether the child was ever stunted
+  # by age category
+  group_by(studyid,subjid,agecat) %>%
+  mutate(evst=ifelse(minhaz< -2,1,0)) 
+
+# count incident cases per study by age
+# exclude time points if number of measurements per age
+# in a study is <50  
+cuminc.data.birth= evs.sens.birth%>%
+  group_by(studyid,agecat) %>%
+  summarise(
+    nchild=length(unique(subjid)),
+    nstudy=length(unique(studyid)),
+    ncases=sum(evst),
+    N=sum(length(evst))) %>%
+  filter(N>=50)
+
+cuminc.data.birth
+
+# estimate random effects, format results
+ci.res.birth=lapply(list("6 months","12 months","18 months","24 months"),function(x)
+  fit.rma(data=cuminc.data.birth,ni="N", xi="ncases",age=x))
+ci.res.birth=as.data.frame(do.call(rbind, ci.res.birth))
+ci.res.birth[,4]=as.numeric(ci.res.birth[,4])
+ci.res.birth$agecat=factor(ci.res.birth$agecat,levels=c("6 months","12 months","18 months","24 months"))
+ci.res.birth$ptest.f=sprintf("%0.02f",ci.res.birth$est)
+
+ci.res.birth
+
+# plot pooled cumulative incidence
+pdf("U:/Figures/stunting-cuminc-pool-bc-nobirth.pdf",width=8,height=3,onefile=TRUE)
+ggplot(ci.res.birth,aes(y=est,x=agecat))+
+  geom_point(size=3)+
+  geom_errorbar(aes(ymin=lb,ymax=ub),width=0.05) +
+  scale_y_continuous(limits=c(0.13,0.8))+
+  xlab("Age category")+
+  ylab("Cumulative incidence (95% CI)")+
+  annotate("text",x=ci.res.birth$agecat,y=0.18,label=ci.res.birth$nmeas.f,size=3)+
+  annotate("text",x=ci.res.birth$agecat,y=0.15,label=ci.res.birth$nstudy.f,size=3)+
+  annotate("text",label=ci.res.birth$ptest.f,x=ci.res.birth$agecat,
+           y=ci.res.birth$est,hjust=-0.3,size=3)+
+  ggtitle("Pooled cumulative incidence of stunting - birth cohorts only - CI excludes birth")
+dev.off()
