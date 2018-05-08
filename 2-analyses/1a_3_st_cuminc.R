2b8b8ff84926bfc8a03f6e517488eaa583f5fb8f
PROD\jade.benjamin-chung <jadebc@berkeley.edu>
2018-05-08 16:49:53 +0000

    add y3, clarify axis label

--- 2-analyses/1a_3_st_cuminc.R
+++ 2-analyses/1a_3_st_cuminc.R
@@ -20,11 +20,12 @@ load("U:/Data/Stunting/stunting_data.RData")
 
 # define age windows
 d = d %>% 
-  mutate(agecat=ifelse(agedays<=6*30.4167,"6 months",
-         ifelse(agedays>6*30.4167 & agedays<=12*30.4167,"12 months",
+  mutate(agecat=ifelse(agedays<=3*30.4167,"3 months",
+       ifelse(agedays>3*30.4167 & agedays<=6*30.4167,"6 months",
+           ifelse(agedays>6*30.4167 & agedays<=12*30.4167,"12 months",
                 ifelse(agedays>12*30.4167 & agedays<=18*30.4167,"18 months",
-                       ifelse(agedays>12*30.4167& agedays<=24*30.4167,"24 months",""))))) %>%
-  mutate(agecat=factor(agecat,levels=c("6 months","12 months","18 months","24 months")))
+                       ifelse(agedays>12*30.4167& agedays<=24*30.4167,"24 months","")))))) %>%
+  mutate(agecat=factor(agecat,levels=c("3 months","6 months","12 months","18 months","24 months")))
 
 # check age categories
 d %>%
@@ -40,10 +41,11 @@ d %>%
     group_by(studyid,subjid) %>%
     arrange(studyid,subjid) %>%
     #create variable with minhaz by age category, cumulatively
-    mutate(minhaz=ifelse(agecat=="6 months",min(haz[agecat=="6 months"]),
-        ifelse(agecat=="12 months",min(haz[agecat=="6 months"|agecat=="12 months"]),
-               ifelse(agecat=="18 months",min(haz[agecat=="6 months"|agecat=="12 months"|agecat=="18 months"]),
-          min(haz))))) %>%
+    mutate(minhaz=ifelse(agecat=="3 months",min(haz[agecat=="3 months"]),
+      ifelse(agecat=="6 months",min(haz[agecat=="3 months" | agecat=="6 months"]),
+        ifelse(agecat=="12 months",min(haz[agecat=="3 months" | agecat=="6 months"|agecat=="12 months"]),
+               ifelse(agecat=="18 months",min(haz[agecat=="3 months" | agecat=="6 months"|agecat=="12 months"|agecat=="18 months"]),
+          min(haz)))))) %>%
     # create indicator for whether the child was ever stunted
     # by age category
     group_by(studyid,agecat,subjid) %>%
@@ -65,28 +67,33 @@ cuminc.data= evs%>%
 cuminc.data
   
 # estimate random effects, format results
-ci.res=lapply(list("6 months","12 months","18 months","24 months"),function(x)
+ci.res=lapply(list("3 months","6 months","12 months","18 months","24 months"),function(x)
     fit.rma(data=cuminc.data,ni="N", xi="ncases",age=x))
 ci.res=as.data.frame(do.call(rbind, ci.res))
 ci.res[,4]=as.numeric(ci.res[,4])
 ci.res = ci.res %>%
   mutate(est=est*100, lb=lb*100, ub=ub*100)
-ci.res$agecat=factor(ci.res$agecat,levels=c("6 months","12 months","18 months","24 months"))
+ci.res$agecat.f=as.factor(ifelse(ci.res$agecat=="3 months","0-3 months",
+                          ifelse(ci.res$agecat=="6 months","4-6 months",
+                                 ifelse(ci.res$agecat=="12 months","7-12 months",
+                                        ifelse(ci.res$agecat=="18 months","13-18 months","19-24 months")))))
+ci.res$agecat.f=factor(ci.res$agecat.f,levels=c("0-3 months","4-6 months",
+      "7-12 months","13-18 months","19-24 months"))
 ci.res$ptest.f=sprintf("%0.0f",ci.res$est)
 
 ci.res
 
 # plot pooled cumulative incidence
 pdf("U:/Figures/stunting-cuminc-pool.pdf",width=8,height=3.5,onefile=TRUE)
-ggplot(ci.res,aes(y=est,x=agecat))+
+ggplot(ci.res,aes(y=est,x=agecat.f))+
   geom_point(size=3)+
   geom_errorbar(aes(ymin=lb,ymax=ub),width=0.05) +
   scale_y_continuous(limits=c(0,80))+
   xlab("Age category")+
   ylab("Cumulative incidence per 100 children (95% CI)")+
-  annotate("text",x=ci.res$agecat,y=5,label=ci.res$nmeas.f,size=3)+
-  annotate("text",x=ci.res$agecat,y=1,label=ci.res$nstudy.f,size=3)+
-  annotate("text",label=ci.res$ptest.f,x=ci.res$agecat,
+  annotate("text",x=ci.res$agecat.f,y=5,label=ci.res$nmeas.f,size=3)+
+  annotate("text",x=ci.res$agecat.f,y=1,label=ci.res$nstudy.f,size=3)+
+  annotate("text",label=ci.res$ptest.f,x=ci.res$agecat.f,
            y=ci.res$est,hjust=-0.75,size=3)+
   ggtitle("Pooled cumulative incidence of stunting")
 dev.off()
@@ -109,10 +116,11 @@ evs.sens.nobirth = left_join(d, bc, by=c("studyid","country")) %>%
   group_by(studyid,subjid) %>%
   arrange(studyid,subjid) %>%
   #create variable with minhaz by age category, cumulatively
-  mutate(minhaz=ifelse(agecat=="6 months",min(haz[agecat=="6 months"]),
-                       ifelse(agecat=="12 months",min(haz[agecat=="6 months"|agecat=="12 months"]),
-                              ifelse(agecat=="18 months",min(haz[agecat=="6 months"|agecat=="12 months"|agecat=="18 months"]),
-                                     min(haz))))) %>%
+  mutate(minhaz=ifelse(agecat=="3 months",min(haz[agecat=="3 months"]),
+                       ifelse(agecat=="6 months",min(haz[agecat=="3 months" | agecat=="6 months"]),
+                              ifelse(agecat=="12 months",min(haz[agecat=="3 months" | agecat=="6 months"|agecat=="12 months"]),
+                                     ifelse(agecat=="18 months",min(haz[agecat=="3 months" | agecat=="6 months"|agecat=="12 months"|agecat=="18 months"]),
+                                            min(haz)))))) %>%
   # create indicator for whether the child was ever stunted
   # by age category
   group_by(studyid,agecat,subjid) %>%
@@ -134,13 +142,18 @@ cuminc.data.nobirth= evs.sens.nobirth%>%
 cuminc.data.nobirth
 
 # estimate random effects, format results
-ci.res.nobirth=lapply(list("6 months","12 months","18 months","24 months"),function(x)
+ci.res.nobirth=lapply(list("3 months","6 months","12 months","18 months","24 months"),function(x)
   fit.rma(data=cuminc.data.nobirth,ni="N", xi="ncases",age=x))
 ci.res.nobirth=as.data.frame(do.call(rbind, ci.res.nobirth))
 ci.res.nobirth[,4]=as.numeric(ci.res.nobirth[,4])
 ci.res.nobirth = ci.res.nobirth %>%
   mutate(est=est*100, lb=lb*100, ub=ub*100)
-ci.res.nobirth$agecat=factor(ci.res.nobirth$agecat,levels=c("6 months","12 months","18 months","24 months"))
+ci.res.nobirth$agecat.f=as.factor(ifelse(ci.res.nobirth$agecat=="3 months","0-3 months",
+                                         ifelse(ci.res.nobirth$agecat=="6 months","4-6 months",
+                                                ifelse(ci.res.nobirth$agecat=="12 months","7-12 months",
+                                                       ifelse(ci.res.nobirth$agecat=="18 months","13-18 months","19-24 months")))))
+ci.res.nobirth$agecat.f=factor(ci.res.nobirth$agecat.f,levels=c("0-3 months","4-6 months",
+                                                                "7-12 months","13-18 months","19-24 months"))
 ci.res.nobirth$ptest.f=sprintf("%0.0f",ci.res.nobirth$est)
 
 ci.res.nobirth
@@ -148,15 +161,15 @@ ci.res.nobirth
 
 # plot pooled cumulative incidence
 pdf("U:/Figures/stunting-cuminc-pool-bc-nobirth.pdf",width=8,height=3.5,onefile=TRUE)
-ggplot(ci.res.nobirth,aes(y=est,x=agecat))+
+ggplot(ci.res.nobirth,aes(y=est,x=agecat.f))+
   geom_point(size=3)+
   geom_errorbar(aes(ymin=lb,ymax=ub),width=0.05) +
   scale_y_continuous(limits=c(0,80))+
   xlab("Age category")+
   ylab("Cumulative incidence per 100 children (95% CI)")+
-  annotate("text",x=ci.res.nobirth$agecat,y=5,label=ci.res.nobirth$nmeas.f,size=3)+
-  annotate("text",x=ci.res.nobirth$agecat,y=1,label=ci.res.nobirth$nstudy.f,size=3)+
-  annotate("text",label=ci.res.nobirth$ptest.f,x=ci.res.nobirth$agecat,
+  annotate("text",x=ci.res.nobirth$agecat.f,y=5,label=ci.res.nobirth$nmeas.f,size=3)+
+  annotate("text",x=ci.res.nobirth$agecat.f,y=1,label=ci.res.nobirth$nstudy.f,size=3)+
+  annotate("text",label=ci.res.nobirth$ptest.f,x=ci.res.nobirth$agecat.f,
            y=ci.res.nobirth$est,hjust=-0.75,size=3)+
   ggtitle("Pooled cumulative incidence of stunting - birth cohorts only - CI excludes birth")
 dev.off()
@@ -169,10 +182,11 @@ evs.sens.birth = left_join(d, bc, by=c("studyid","country")) %>%
   group_by(studyid,subjid) %>%
   arrange(studyid,subjid) %>%
   #create variable with minhaz by age category, cumulatively
-  mutate(minhaz=ifelse(agecat=="6 months",min(haz[agecat=="6 months"]),
-                       ifelse(agecat=="12 months",min(haz[agecat=="6 months"|agecat=="12 months"]),
-                              ifelse(agecat=="18 months",min(haz[agecat=="6 months"|agecat=="12 months"|agecat=="18 months"]),
-                                     min(haz))))) %>%
+  mutate(minhaz=ifelse(agecat=="3 months",min(haz[agecat=="3 months"]),
+                       ifelse(agecat=="6 months",min(haz[agecat=="3 months" | agecat=="6 months"]),
+                              ifelse(agecat=="12 months",min(haz[agecat=="3 months" | agecat=="6 months"|agecat=="12 months"]),
+                                     ifelse(agecat=="18 months",min(haz[agecat=="3 months" | agecat=="6 months"|agecat=="12 months"|agecat=="18 months"]),
+                                            min(haz)))))) %>%
   # create indicator for whether the child was ever stunted
   # by age category
   group_by(studyid,agecat,subjid) %>%
@@ -194,28 +208,33 @@ cuminc.data.birth= evs.sens.birth%>%
 cuminc.data.birth
 
 # estimate random effects, format results
-ci.res.birth=lapply(list("6 months","12 months","18 months","24 months"),function(x)
+ci.res.birth=lapply(list("3 months","6 months","12 months","18 months","24 months"),function(x)
   fit.rma(data=cuminc.data.birth,ni="N", xi="ncases",age=x))
 ci.res.birth=as.data.frame(do.call(rbind, ci.res.birth))
 ci.res.birth[,4]=as.numeric(ci.res.birth[,4])
 ci.res.birth = ci.res.birth %>%
   mutate(est=est*100, lb=lb*100, ub=ub*100)
-ci.res.birth$agecat=factor(ci.res.birth$agecat,levels=c("6 months","12 months","18 months","24 months"))
+ci.res.birth$agecat.f=as.factor(ifelse(ci.res.birth$agecat=="3 months","0-3 months",
+                                       ifelse(ci.res.birth$agecat=="6 months","4-6 months",
+                                              ifelse(ci.res.birth$agecat=="12 months","7-12 months",
+                                                     ifelse(ci.res.birth$agecat=="18 months","13-18 months","19-24 months")))))
+ci.res.birth$agecat.f=factor(ci.res.birth$agecat.f,levels=c("0-3 months","4-6 months",
+                                                            "7-12 months","13-18 months","19-24 months"))
 ci.res.birth$ptest.f=sprintf("%0.0f",ci.res.birth$est)
 
 ci.res.birth
 
 # plot pooled cumulative incidence
 pdf("U:/Figures/stunting-cuminc-pool-bc-birth.pdf",width=8,height=3.5,onefile=TRUE)
-ggplot(ci.res.birth,aes(y=est,x=agecat))+
+ggplot(ci.res.birth,aes(y=est,x=agecat.f))+
   geom_point(size=3)+
   geom_errorbar(aes(ymin=lb,ymax=ub),width=0.05) +
   scale_y_continuous(limits=c(0,80))+
   xlab("Age category")+
   ylab("Cumulative incidence per 100 children (95% CI)")+
-  annotate("text",x=ci.res.birth$agecat,y=5,label=ci.res.birth$nmeas.f,size=3)+
-  annotate("text",x=ci.res.birth$agecat,y=1,label=ci.res.birth$nstudy.f,size=3)+
-  annotate("text",label=ci.res.birth$ptest.f,x=ci.res.birth$agecat,
+  annotate("text",x=ci.res.birth$agecat.f,y=5,label=ci.res.birth$nmeas.f,size=3)+
+  annotate("text",x=ci.res.birth$agecat.f,y=1,label=ci.res.birth$nstudy.f,size=3)+
+  annotate("text",label=ci.res.birth$ptest.f,x=ci.res.birth$agecat.f,
            y=ci.res.birth$est,hjust=-0.75,size=3)+
   ggtitle("Pooled cumulative incidence of stunting - birth cohorts only - CI includes birth")
 dev.off()
