d82ed12c72ee8a9f8f7c51972c3d65f5ab91f604
PROD\jade.benjamin-chung <jadebc@berkeley.edu>
2018-05-09 15:38:50 +0000

    finished coding first take of reversal

--- 2-analyses/1b_1_st_catchup.R
+++ 2-analyses/1b_1_st_catchup.R
@@ -6,21 +6,10 @@
 
 # Cohort specific estimates & 
 # Pooled estimates using random effects
+
+# What is the proportion of stunting at birth, 3, 6, and 12, and 
+# 18 mos. of age that is reversible? 
 #-----------------------------------
-
-# will be defined as a LAZ change from below to -2 or greater 
-# among children who are currently stunted or severely stunted. 
-# We will estimate stunting reversal by each age of interest and define
-# stunting as reversed if a child’s LAZ measurements change from < -2 
-# to >= -2 by that age, ignoring any changes in LAZ at future older
-# ages. Children will only be considered “at risk” for reversal 
-# after their LAZ falls below -2. 
-
-
-
-# What is the proportion 
-# reversible after 3, 6, 9, 12 and 18 mos. of age?
-
 # What is the 
 # mean duration of time for reversing stunting (measured in cohorts 
 # with at least monthly measurement)? 
@@ -28,6 +17,11 @@
 # What is the proportion of 
 # children who were ever stunted within the first 2 years of life, 
 # who are stunted (or not) at 24, 36, and 48 mos. of age?
+
+# OPEN ISSUE: there are multiple measurements for the same kid in jvita3, maybe others
+# waiting on andrew's response 
+# > rev3[rev3$studyid=="kiGH5241-JiVitA-3" & rev3$subjid==9683,
+#        +      c("subjid","measid","agedays","agecat","haz")]
 
 rm(list=ls())
 library(dplyr)
@@ -59,8 +53,10 @@ d %>%
             mean=mean(agedays/30.4167,na.rm=TRUE),
             max=max(agedays/30.4167))
 
+#-----------------------------------------------
 # What is the proportion of stunting at birth, 3, 6, and 12, and 
 # 18 mos. of age that is reversible? 
+#-----------------------------------------------
 
 # identify children with catchup growth
 rev = d %>%
@@ -90,38 +86,115 @@ rev = d %>%
   # flag each time haz>=-2
   mutate(rec_age=ifelse(atrisk==1 & haz>=-2 & meas_age==maxmeas_age,1,0))
   
+# identify first recovery episode
 rev.child= rev %>%
-  # identify first recovery episode
-  group_by(studyid,agecat,subjid) %>%
+  group_by(studyid,subjid) %>%
   filter(rec_age==1) %>%
-  mutate(countrec=seq_along(subjid)) %>%
-  filter(countrec==1) 
-  
-# next steps: figure out why countrec is not working 
-# as a sequence. once it's working, subset to conutrec=1
-# then maybe merge the rev.child back on
+  mutate(firstrec=seq_along(subjid)) %>%
+  filter(firstrec==1) %>%
+  select(studyid, subjid,agedays,firstrec)
 
-         firstrec=
-         rec_3m=ifelse(rec_age)) 
-  
+# indicator for incident case in age group, flag
+# on last measurement in that age group
+inc.child= rev %>%
+  group_by(studyid,subjid,agecat) %>%
+  summarise(inc_in_age=max(inccase)) %>%
+  mutate(inc_age=ifelse(inc_in_age==1,as.character(agecat),""))
 
-rev.sum= rev %>%
-  group_by(studyid,country,subjid) %>%
-  summarise(rec3m=)
+# merge indicator for first recovery back onto main data
+rev2= left_join(rev,rev.child,by=c("studyid","subjid","agedays")) %>%
+  mutate(firstrec=ifelse(is.na(firstrec),0,1)) 
+
+# merge on indicator for incidence case in age group 
+rev3 = left_join(rev2, inc.child, by=c("studyid","subjid","agecat")) %>%
+  # recovery at 3 months for stunting at birth
+  mutate(rec3m=ifelse(agecat=="3 months" & firstrec==1 & inc_age=="3 months",1,0),
+         rec6m=ifelse(agecat=="6 months" & firstrec==1 & inc_age=="6 months",1,0),
+         rec9m=ifelse(agecat=="9 months" & firstrec==1 & inc_age=="9 months",1,0),
+         rec12m=ifelse(agecat=="12 months" & firstrec==1 & inc_age=="12 months",1,0),
+         rec18m=ifelse(agecat=="18 months" & firstrec==1 & inc_age=="18 months",1,0),
+         rec24m=ifelse(agecat=="24 months" & firstrec==1 & inc_age=="24 months",1,0))
+  
+rev.data=rev3 %>%
+  # child level data 
+  group_by(agecat,studyid,country,subjid) %>%
+  summarise_at(.vars=c("rec3m", "rec6m", "rec12m", "rec18m", "rec24m"),
+               .funs=max) %>%
+  # subset to kids at risk
+  filter(atirsk==1) %>%
+  # organize into single column
+  mutate(rec=ifelse(rec3m==1,1,
+                    ifelse(rec6m==1,1,
+                           ifelse(rec12m==1,1,
+                                  ifelse(rec18m==1,1,
+                                         ifelse(rec24m==1,1,0)))))) %>%
+  # study specific means
+  group_by(studyid,country,agecat) %>%
+  summarise(n.rec=length(subjid[rec==1]),N=n())
+
+
+# estimate random effects, format results
+rev.res=lapply(list("3 months","6 months","12 months","18 months","24 months"),function(x) 
+  fit.rma(rev.data,ni="N", xi="n.rec",age=x))
+rev.res=as.data.frame(do.call(rbind, rev.res))
+rev.res[,4]=as.numeric(rev.res[,4])
+rev.res = rev.res %>%
+  mutate(est=est*100,lb=lb*100,ub=ub*100)
+rev.res$agecat=factor(rev.res$agecat,levels=c("Birth","3 months","6 months","12 months","18 months","24 months"))
+rev.res$ptest.f=sprintf("%0.0f",rev.res$est)
+
+rev.res
+
+# NOTE THE PLOT SAYS MEASUREMENTS BUT IT IS KIDS< NEED TO FIX FUNCTION
+
+# plot % recovered by age
+pdf("U:/Figures/stunting-rec-pool.pdf",width=10,height=4,onefile=TRUE)
+ggplot(rev.res,aes(y=est,x=agecat))+
+  geom_point(size=3)+
+  geom_errorbar(aes(ymin=lb,ymax=ub),width=0.05) +
+  scale_color_manual(values=tableau10)+xlab("Age category")+
+  ylab("Percentage (95% CI)")+
+  scale_y_continuous(limits=c(0,4))+
+  annotate("text",x=rev.res$agecat,y=0.3,label=rev.res$nmeas.f,size=3)+
+  annotate("text",x=rev.res$agecat,y=0,label=rev.res$nstudy.f,size=3)+
+  annotate("text",label=rev.res$ptest.f,x=rev.res$agecat,
+           y=rev.res$est,hjust=-1.1,size=3)+
+  ggtitle("Percentage of children who recovered from stunting")
+dev.off()
+
   
 rev[rev$studyid=="ki1000108-CMC-V-BCS-2002" & rev$subjid==9,
     c("subjid","measid","agedays","agecat","haz","inccase","atrisk",
-      "meas_age","maxmeas_age","rec_age","firstrec")][1:20,]
+      "meas_age","maxmeas_age","rec_age")][1:20,]
 
 rev.child[rev.child$studyid=="ki1000108-CMC-V-BCS-2002" & rev.child$subjid==9,
           c("subjid","measid","agedays","agecat","haz","inccase","atrisk",
             "rec_age","countrec")]
+inc.child[inc.child$studyid=="ki1000108-CMC-V-BCS-2002" & inc.child$subjid==9,]
+
+
+
+rev2[rev2$studyid=="ki1000108-CMC-V-BCS-2002" & rev2$subjid==9,
+    c("subjid","measid","agedays","agecat","haz","inccase","atrisk",
+      "meas_age","maxmeas_age","rec_age","firstrec")][1:20,]
+
+rev3[rev3$studyid=="ki1000108-CMC-V-BCS-2002" & rev3$subjid==9,
+     c("subjid","measid","agedays","agecat","haz","inccase","atrisk",
+       "meas_age","maxmeas_age","rec_age","firstrec","inc_age",
+       "rec3m","rec6m")][1:20,]
+rev3[rev3$studyid=="ki1000108-CMC-V-BCS-2002" & rev3$subjid==12,
+     c("subjid","measid","agedays","agecat","haz","inccase","atrisk",
+       "meas_age","maxmeas_age","rec_age","firstrec","inc_age",
+       "rec3m","rec6m")][1:20,]
 
 # recovered
 rev[rev$studyid=="ki1000108-CMC-V-BCS-2002" & rev$subjid==1,
     c("subjid","measid","agedays","agecat","haz","inccase","atrisk",
       "meas_age","maxmeas_age","rec3m_row","rec3m_age")][1:20,]
 
-
+rev3[rev3$studyid=="kiGH5241-JiVitA-3" & rev3$subjid==9683,
+     c("subjid","measid","agedays","agecat","haz","inccase","atrisk",
+       "meas_age","maxmeas_age","rec_age","firstrec","inc_age",
+       "rec3m","rec6m")]
   
   
