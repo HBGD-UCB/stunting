4de79f79f85fbdf4abf9fe79147bb94edddc027d
PROD\jade.benjamin-chung <jadebc@berkeley.edu>
2018-06-01 18:23:44 +0000

    add recovery

--- 1-dm/6_merge_outcomes_and_covariates.R
+++ 1-dm/6_merge_outcomes_and_covariates.R
@@ -4,7 +4,8 @@ library(tidyverse)
 
 #merge outcomes with covariates
 
-setwd("U:/UCB-SuperLearner/Stunting rallies/")
+# setwd("U:/UCB-SuperLearner/Stunting rallies/")
+setwd("U:/ucb-superlearner/Stunting rallies/")
 
 #load covariates
 cov<-readRDS("FINAL_temp_clean_covariates.rds")
@@ -12,22 +13,28 @@ cov<-readRDS("FINAL_temp_clean_covariates.rds")
 #load outcomes
 load("st_prev.rdata")
 load("st_cuminc.rdata")
+load("st_rec.rdata")
 
 
 dim(prev)
 dim(cuminc)
+dim(rev)
 
 
 colnames(prev)
 colnames(cuminc)
+colnames(rev)
 
 
 head(prev)
 head(cuminc)
+head(rev)
 
 #convert subjid to character for the merge with covariate dataset
+cov$subjid <- as.character(cov$subjid)
 prev$subjid <- as.character(prev$subjid)
 cuminc$subjid <- as.character(cuminc$subjid)
+rev$subjid <- as.character(rev$subjid)
 
 
 #------------------------------------
@@ -77,3 +84,35 @@ Y<-c("stunted","sstunted")
 
 
 save(d, Y, A,V, id, file="st_prev_rf.Rdata")
+
+
+#------------------------------------
+# Create recovery dataset
+#------------------------------------
+
+#merge in covariates
+d <- left_join(rev, cov, by=c("studyid", "subjid", "country"))
+head(d)
+
+
+#Vector of outcome names
+Y<-c("s03rec24")
+
+#Vector of risk factor names
+A<-c( "sex",              "gagebrth",      "birthwt",      
+      "birthlen",      "enstunt",       "vagbrth",       "hdlvry",        "mage",          "mhtcm",         "mwtkg",        
+      "mbmi",          "single",        "fage",          "fhtcm",         "nrooms",        "nhh",           "nchldlt5",     
+      "hhwealth_quart", "month", "brthmon", "parity",   "meducyrs", 
+      "feducyrs", "hfoodsec")
+#Vector of covariate names
+W<-c("")
+
+#Subgroup variable
+V <- c("agecat")
+
+#clusterid ID variable
+id <- c("id")
+
+
+save(d, Y, A,V, id, file="st_rec_rf.Rdata")
+
